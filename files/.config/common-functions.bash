c09cd0e7fe23e9170cc2a8d205802f7b9b82b583
Chris Philip <cmgp@google.com>
2019-08-08 15:52:51 -0700

    Update common-functions.bash

--- files/.config/common-functions.bash
+++ files/.config/common-functions.bash
@@ -255,6 +255,33 @@ function _column_print() {
   sort | COLUMNS=$(stty -a <"/dev/pts/1" | grep -Po '(?<=columns )\d+') ~/.config/scripts/column-print.py
 }                                                                                                   
 
+function make-completion-wrapper () {
+# Author.: Ole J
+# Date...: 23.03.2008
+# License: Whatever
+
+# Wraps a completion function
+# make-completion-wrapper <actual completion function> <name of new func.>
+#                         <command name> <list supplied arguments>
+# eg.
+#   alias agi='apt-get install'
+#   make-completion-wrapper _apt_get _apt_get_install apt-get install
+# defines a function called _apt_get_install (that's $2) that will complete
+# the 'agi' alias. (complete -F _apt_get_install agi)
+    local function_name="$2"
+    local arg_count=$(($#-3))
+    local comp_function_name="$1"
+    shift 2
+    local function="
+function $function_name {
+    ((COMP_CWORD+=$arg_count))
+    COMP_WORDS=( "$@" \${COMP_WORDS[@]:1} )
+    "$comp_function_name"
+    return 0
+}"
+    eval "$function"
+}
+
 # Source-able file to load all functions into shell
 # Executable file that to run functions from not a shell
 # Functions without "^function " are 'private' and not runnable externally
